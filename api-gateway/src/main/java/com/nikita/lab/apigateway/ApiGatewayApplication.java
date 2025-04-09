3bfd939f050242769374ee414fc201244a514719
returnaction <obergannv@gmail.com>
2025-04-09 18:24:29 +0300

    Ничего не работает 10 часов и все равно на порту 8080 не пробрасываетс а на user-service все нормлаьно

---
+++ api-gateway/src/main/java/com/nikita/lab/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nikita.lab.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ user-service/src/test/java/com/nikita/lab/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nikita.lab.userservice;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@Disabled
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
